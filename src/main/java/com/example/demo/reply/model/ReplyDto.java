707ec8a7998fd9679e001367339f333bbab661e2
Beomsoo <linoleca@naver.com>
2026-02-26 10:45:43 +0900

    [Feat] 댓글 작성시 로그인한 작성자, 작성하고 있는 게시글 정보 저장되도록 구현

--- src/main/java/com/example/demo/reply/model/ReplyDto.java
+++ src/main/java/com/example/demo/reply/model/ReplyDto.java
@@ -1,10 +1,30 @@
 package com.example.demo.reply.model;
 
+import com.example.demo.board.model.Board;
+import com.example.demo.user.model.User;
 import lombok.Builder;
 import lombok.Getter;
 
 
 public class ReplyDto {
+
+    @Getter
+    public static class ReplyReq {
+        private String contents;
+
+        public Reply toEntity(Long userIdx, Long boardIdx) {
+            return Reply.builder()
+                    .contents(this.contents)
+                    .user(User.builder()
+                            .idx(userIdx)
+                            .build())
+                    .board(Board.builder()
+                            .idx(boardIdx)
+                            .build())
+                    .build();
+        }
+    }
+
     @Builder
     @Getter
     public static class ReplyRes {
