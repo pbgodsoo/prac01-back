9822f2c7bd48f104756a64c202c7961e2e5e4ac3
Beomsoo <linoleca@naver.com>
2026-02-26 14:19:33 +0900

    [Feat] 좋아요 기능 구현(user, board)

--- src/main/java/com/example/demo/likes/LikesRepository.java
+++ src/main/java/com/example/demo/likes/LikesRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.likes;
 
-public class LikesRepository {
+import com.example.demo.likes.model.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikesRepository extends JpaRepository<Likes, Long> {
 }
