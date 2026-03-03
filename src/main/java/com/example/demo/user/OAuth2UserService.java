4d1478696e6eb0a6322aa63b2f04ec3206cc9279
Beomsoo <linoleca@naver.com>
2026-03-03 12:07:06 +0900

    [Refactor] 시큐리티의 기본 DTO에서 우리 서비스의 DTO로 변환해서 처리

--- src/main/java/com/example/demo/user/OAuth2UserService.java
+++ src/main/java/com/example/demo/user/OAuth2UserService.java
@@ -2,22 +2,20 @@ package com.example.demo.user;
 
 import com.example.demo.user.model.AuthUserDetails;
 import com.example.demo.user.model.User;
+import com.example.demo.user.model.UserDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class OAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
-
-    public OAuth2UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -25,34 +23,18 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
         // OAuth2 로그인 실행
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        String providerId = ((Long)attributes.get("id")).toString();
-        System.out.println(providerId);
-
-        String email = providerId + "@kakao.social";
-        Map properties = (Map) attributes.get("properties");
-        String name = (String) properties.get("nickname");
+        // 내 서비스의 DTO로 변환
+        UserDto.OAuth dto = UserDto.OAuth.from(oAuth2User.getAttributes(), "kakao");
 
         // DB에 화면이 있나 없나 확인
-        Optional<User> result = userRepository.findByEmail(email);
+        Optional<User> result = userRepository.findByEmail(dto.getEmail());
 
         // 없으면 가입 시켜주기
-        // 없으면 가입 시켜주기
-        User user = null;
-
         if (!result.isPresent()) {
-            user = userRepository.save(
-                    User.builder()
-                            .email(email)
-                            .name(name)
-                            .password("kakao")
-                            .enable(true)
-                            .role("ROLE_USER")
-                            .build()
-            );
+            User user = userRepository.save(dto.toEntity());
             return AuthUserDetails.from(user);
         } else {
-            user = result.get();
+            User user = result.get();
             return AuthUserDetails.from(user);
         }
     }
