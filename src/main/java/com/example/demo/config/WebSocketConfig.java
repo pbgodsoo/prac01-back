5a2f751ef5feae3f4c50ad747a837959d98ef8db
Beomsoo <linoleca@naver.com>
2026-03-07 14:57:53 +0900

    [Feat] 웹 소켓 서버 설정 및 클라이언트 연결까지 구현

---
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import com.example.demo.config.websocket.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
+    }
+}
